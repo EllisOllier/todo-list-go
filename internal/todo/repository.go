46d1c23ca9c7f08a8b1acbabbb722e475790979c
EllisOllier <ellisollier1@gmail.com>
2026-01-12 20:38:01 +0000

    Update comment to reflect purpose of file more accurately

--- internal/todo/repository.go
+++ internal/todo/repository.go
@@ -1,3 +1,5 @@
 package todo
 
 // add database integration with sqlite
+// will run sql queries which are handled by the handler.go
+// works very similar to controller (handler.go) and model (repository.go) architecture
