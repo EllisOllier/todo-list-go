a098a7d033b2e6bef3d82659ce5a7fdd46c96a8d
EllisOllier <ellisollier1@gmail.com>
2026-01-12 11:08:25 +0000

    Update comments to document code better

--- main.go
+++ main.go
@@ -22,19 +22,27 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 
 func postTodo(w http.ResponseWriter, r *http.Request) {
 	var newTodo Todo
-	var mu sync.Mutex // sync.Mutex is a type not a value
-	dec := json.NewDecoder(r.Body)
+	var mu sync.Mutex              // sync.Mutex is a type not a value
+	dec := json.NewDecoder(r.Body) // := requires a specified value on the right side
 
 	if err := dec.Decode(&newTodo); err != nil {
 		return
 	}
 
-	mu.Lock()
-	todos = append(todos, newTodo)
-	mu.Unlock()
+	mu.Lock()                      // locks the goroutine so todos slice can only be modified at this point
+	todos = append(todos, newTodo) // appends newTodo from body to todos
+	mu.Unlock()                    // unlocks the goroutine and allows todos slice to be modified
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newTodo)
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+
 }
 
 type Todo struct {
