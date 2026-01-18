3b75ccc35806528b195c2e7a484b02713f87eb6b
EllisOllier <ellisollier1@gmail.com>
2026-01-18 12:27:50 +0000

    Remove unused imports and comment

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -2,12 +2,9 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 
 	_ "github.com/lib/pq"
 )
-
-// handle database connection
 
 func Connect() (*sql.DB, error) {
 	connStr := "user=postgres password=Golang2026! dbname=postgres port=5431 sslmode=disable"
@@ -20,8 +17,6 @@ func Connect() (*sql.DB, error) {
 	if err != nil {
 		return db, err
 	}
-
-	fmt.Println("Successfully connected to todo-database container")
 
 	return db, err
 }
