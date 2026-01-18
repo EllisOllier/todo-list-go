7b3156c10d510f09f20bcb91c0db23bc5537182c
EllisOllier <ellisollier1@gmail.com>
2026-01-18 13:07:42 +0000

    Update comment on NewTodoService

--- internal/todo/model.go
+++ internal/todo/model.go
@@ -11,9 +11,9 @@ type TodoService struct {
 	todoRepository *TodoRepository
 }
 
-// a helper function to initialise and handle Todos slice which
-// allows a single instance to be saved in memory and accessed from
-// both /cmd/server/main.go and /internal/todo/handler.go
+// a helper function to initialise and handle database interaction with TodoRepository
+// which allows the data from the database to be saved in memory
+// and accessed from both /cmd/server/main.go and /internal/todo/handler.go
 // with the same dataset syncing across
 func NewTodoService(givenTodoRepository *TodoRepository) *TodoService {
 	return &TodoService{
