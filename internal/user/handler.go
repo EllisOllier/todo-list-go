5eb1670096d9dd6c83e3dda536c49508d96738c4
EllisOllier <ellisollier1@gmail.com>
2026-01-19 15:08:31 +0000

    Add login handler

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -3,6 +3,8 @@ package user
 import (
 	"encoding/json"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *UserService) CreateAccount(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +37,46 @@ func (s *UserService) CreateAccount(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	newUser.ID = userId
-	newUser.PasswordHash = ""
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newUser)
+}
+
+func (s *UserService) Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type GivenUserRequest struct {
+		Username     *string `json:"username"`
+		PasswordHash *string `json:"password_hash"`
+	}
+	var req GivenUserRequest
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&req)
+	if err != nil {
+		http.Error(w, "Bad Request: 400", http.StatusBadRequest)
+		return
+	}
+	if req.Username == nil {
+		http.Error(w, "Missing username field in body", http.StatusBadRequest)
+		return
+
+	}
+	if req.PasswordHash == nil {
+		http.Error(w, "Missing password_hash field in body", http.StatusBadRequest)
+		return
+	}
+
+	givenUser := User{Username: *req.Username, PasswordHash: *req.PasswordHash}
+	userId, err := s.userRepository.Login(givenUser)
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			http.Error(w, "Incorrect Password", http.StatusBadRequest)
+			return
+		}
+		http.Error(w, "Server Error: 500", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(userId)
 }
