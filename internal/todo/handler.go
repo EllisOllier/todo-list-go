65121b3efebdc90261f4fc3e9d19e95271dd5d6e
EllisOllier <ellisollier1@gmail.com>
2026-01-21 16:15:20 +0000

    Add user validation to handlers

--- internal/todo/handler.go
+++ internal/todo/handler.go
@@ -6,13 +6,27 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+
+	"github.com/EllisOllier/todo-list-go/internal/middleware"
 )
+
+type TodoRequest struct {
+	Task *string `json:"task"`
+	Done *bool   `json:"done"`
+}
 
 // runs GET request for all todos slice stored in-memory
 func (s *TodoService) GetTodos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
+	}
+
 	enc := json.NewEncoder(w) // set NewEncoder to use ResponseWriter as the output stream
-	rows, err := s.todoRepository.GetAllTodos()
+	rows, err := s.todoRepository.GetAllTodos(userId)
 	if err != nil {
 		http.Error(w, "Server Error: 500", http.StatusInternalServerError)
 		return
@@ -48,11 +62,14 @@ func (s *TodoService) GetTodoById(w http.ResponseWriter, r *http.Request) {
 // runs POST request to add new a task to todos slice stored in-memory
 func (s *TodoService) PostTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	type NewTodoRequest struct {
-		Task *string `json:"task"`
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
 	}
 
-	var req NewTodoRequest
+	var req TodoRequest
 
 	dec := json.NewDecoder(r.Body) // := requires a specified value on the right side
 	if err := dec.Decode(&req); err != nil {
@@ -63,14 +80,16 @@ func (s *TodoService) PostTodo(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Missing task field", http.StatusBadRequest)
 		return
 	}
-	newTodo := Todo{Task: *req.Task}
 
-	todoId, err := s.todoRepository.AddTodo(newTodo)
+	isDone := false // always false as new todo
+	newTodo := Todo{Task: req.Task, Done: &isDone, UserId: userId}
+
+	todoId, err := s.todoRepository.AddTodo(newTodo, userId)
 	if err != nil {
 		http.Error(w, "Server Error: 500", http.StatusInternalServerError)
 		return
 	}
-	newTodo.ID = todoId
+	newTodo.ID = *todoId
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newTodo)
@@ -79,8 +98,11 @@ func (s *TodoService) PostTodo(w http.ResponseWriter, r *http.Request) {
 // runs PATCH request to replace a todo item matching the given id
 func (s *TodoService) PatchTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	type UpdateTodoRequest struct {
-		Task *string `json:"task"`
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
 	}
 
 	id, err := strconv.Atoi(r.PathValue("id"))
@@ -89,14 +111,14 @@ func (s *TodoService) PatchTodo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var req UpdateTodoRequest
+	var req TodoRequest
 	dec := json.NewDecoder(r.Body)           // decodes the body
 	if err := dec.Decode(&req); err != nil { // fetches the updated todo from the request body
 		return
 	}
 
-	updateTodo := Todo{ID: id, Task: *req.Task}
-	repoErr := s.todoRepository.UpdateTodo(id, updateTodo)
+	updateTodo := Todo{ID: id, Task: req.Task, Done: req.Done}
+	todo, repoErr := s.todoRepository.UpdateTodo(updateTodo, id, userId)
 	if repoErr != nil {
 		if errors.Is(repoErr, sql.ErrNoRows) {
 			http.Error(w, "Not Found: 404", http.StatusNotFound)
@@ -107,18 +129,25 @@ func (s *TodoService) PatchTodo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(updateTodo)
+	json.NewEncoder(w).Encode(todo)
 }
 
 // runs DELETE request to delete a todo item task matching the given id
 func (s *TodoService) DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
+	}
+
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
 		http.Error(w, "Invalid ID format", http.StatusBadRequest)
 		return
 	}
 
-	repoErr := s.todoRepository.DeleteTodo(id)
+	repoErr := s.todoRepository.DeleteTodo(id, userId)
 	if repoErr != nil {
 		if errors.Is(repoErr, sql.ErrNoRows) {
 			http.Error(w, "Not Found: 404", http.StatusNotFound)
