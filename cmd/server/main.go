ab1a409da3bffa7c50d7cc81af4b53f5cbfa56ec
EllisOllier <ellisollier1@gmail.com>
2026-01-21 14:18:57 +0000

    Update to use port from env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	// imported with command: "go mod init github.com/EllisOllier/todo-list-go"
 	"github.com/EllisOllier/todo-list-go/internal/database"
 	"github.com/EllisOllier/todo-list-go/internal/middleware"
 	"github.com/EllisOllier/todo-list-go/internal/todo" // uses repo to import /internal/todo code as they are private
 	"github.com/EllisOllier/todo-list-go/internal/user"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("SERVER_PORT")
+
 	log.Println("Attempting to connect to database...")
 	db, err := database.Connect()
 	if err != nil {
@@ -39,5 +48,5 @@ func main() {
 	mux.HandleFunc("POST /user/login", userService.Login)
 
 	middlewareMux := middleware.LoggingMiddleware(mux) // all routes run through mux
-	http.ListenAndServe(":8080", middlewareMux)
+	http.ListenAndServe(port, middlewareMux)
 }
