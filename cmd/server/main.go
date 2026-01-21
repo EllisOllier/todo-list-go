847aefccb6a5b1daa2dfcb515da792f23376e684
EllisOllier <ellisollier1@gmail.com>
2026-01-21 15:28:29 +0000

    Add auth to DELETE /todos/{id}

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -41,7 +41,7 @@ func main() {
 	mux.HandleFunc("GET /todos/{id}", todoService.GetTodoById)
 	mux.Handle("POST /todos", middleware.Authenticate(http.HandlerFunc(todoService.PostTodo)))
 	mux.HandleFunc("PATCH /todos/{id}", todoService.PatchTodo)
-	mux.HandleFunc("DELETE /todos/{id}", todoService.DeleteTodo)
+	mux.Handle("DELETE /todos/{id}", middleware.Authenticate(http.HandlerFunc(todoService.DeleteTodo)))
 
 	// uses userService to access routes from /internal/user/handler.go
 	mux.HandleFunc("POST /user", userService.CreateAccount)
