5102c968607bd42576f7fee0e77e7e9a762d364d
EllisOllier <ellisollier1@gmail.com>
2026-01-21 14:31:35 +0000

    Add GenerateToken method for UserService

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,5 +1,12 @@
 package user
 
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
 type User struct {
 	ID           int    `json:"id"`
 	Username     string `json:"username"`
@@ -14,4 +21,26 @@ func NewUserService(givenUserRepository *UserRepository) *UserService {
 	return &UserService{
 		userRepository: givenUserRepository,
 	}
+}
+
+func (s *UserService) GenerateToken(user_id int) (string, error) {
+	signingKey := []byte(os.Getenv("JWT_SECRET"))
+
+	type Claims struct {
+		UserId int `json:"user_id"`
+		jwt.RegisteredClaims
+	}
+
+	claims := Claims{
+		user_id,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(72 * time.Hour)),
+			Subject:   string(rune(user_id)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(signingKey)
+
+	return ss, err
 }
